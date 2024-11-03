af9facb1e217ddf112b610fa58ee00542a035f3c
Harilal AV <harilalav1080@gmail.com>
2024-11-03 23:18:52 +0530

    Added movie model and migrations

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.CharField(max_length=255, blank=True, null=True)
+
+
+class MovieCollection(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    movies = models.ManyToManyField(Movie, related_name="movie_collections")
