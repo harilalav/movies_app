3c0e778a6155e24ea74dafb376b3c1b6d2374bfc
Harilal AV <harilalav1080@gmail.com>
2024-11-03 23:33:47 +0530

    updated urls

--- general/urls.py
+++ general/urls.py
@@ -2,11 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("request-count/", views.RequestCountView.as_view(), name="request_count"),
-    path(
-        "request-count/reset/",
-        views.ResetRequestCountView.as_view(),
-        name="reset_request_count",
-    ),
-]
+urlpatterns = []
--- movies_app/urls.py
+++ movies_app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from general.views import RequestCountView, ResetRequestCountView
 from movies.views import MovieCollectionViewSet, MovieListView
 from users.views import RegisterView
 
@@ -13,5 +14,11 @@ urlpatterns = [
     path("general/", include("general.urls")),
     path("register/", RegisterView.as_view(), name="join"),
     path("movies/", MovieListView.as_view(), name="movie_list"),
-    path("collection/", include(router.urls)),
+    path("movies/", MovieListView.as_view(), name="movie_list"),
+    path("request-count/", RequestCountView.as_view(), name="request_count"),
+    path(
+        "request-count/reset/",
+        ResetRequestCountView.as_view(),
+        name="reset_request_count",
+    ),
 ]
