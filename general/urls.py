5110b0e03300c43159df00ec06a8c4c6326270ea
Harilal AV <harilalav1080@gmail.com>
2024-11-03 20:39:41 +0530

    Changed to DRF class based views

--- general/urls.py
+++ general/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("request-count/", views.get_request_count, name="request_count"),
-    path("request-count/reset/", views.reset_request_count, name="reset_request_count"),
+    path("request-count/", views.RequestCountView.as_view(), name="request_count"),
+    path(
+        "request-count/reset/",
+        views.ResetRequestCountView.as_view(),
+        name="reset_request_count",
+    ),
 ]
--- general/views.py
+++ general/views.py
@@ -1,20 +1,26 @@
-# views.py
-
 from django.core.cache import cache
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from movies_app.middlewares import RequestCountMiddleware
-from rest_framework.decorators import api_view
 
 
-@api_view(["GET"])
-def get_request_count(request):
-    # Fetch the current request count from Redis
-    request_count = cache.get(RequestCountMiddleware.REQUEST_COUNT_KEY, 0)
-    return JsonResponse({"requests": request_count})
+class RequestCountView(APIView):
+    """
+    API view to get the total number of requests.
+    """
+
+    def get(self, request):
+        # Fetch the current request count from Redis
+        request_count = cache.get(RequestCountMiddleware.REQUEST_COUNT_KEY, 0)
+        return Response({"requests": request_count})
 
 
-@api_view(["POST"])
-def reset_request_count(request):
-    # Reset the request count in Redis
-    cache.set(RequestCountMiddleware.REQUEST_COUNT_KEY, 0)
-    return JsonResponse({"message": "request count reset successfully"})
+class ResetRequestCountView(APIView):
+    """
+    API view to reset the request count.
+    """
+
+    def post(self, request):
+        # Reset the request count in Redis
+        cache.set(RequestCountMiddleware.REQUEST_COUNT_KEY, 0)
+        return Response({"message": "request count reset successfully"})
